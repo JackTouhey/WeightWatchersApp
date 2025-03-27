62bfaa8b13f4c0d1faa47d577f7cf8af7658e6eb
Jack Touhey <jt112112@gmail.com>
2025-03-28 12:10:22 +1300

    #2 Implemented HistoryAdapter class

---
+++ app/src/main/java/com/example/weightwatchersapp/HistoryAdapter.java
@@ -0,0 +1,63 @@
+package com.example.weightwatchersapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder> {
+    private ArrayList<Day> history;
+
+    public HistoryAdapter(ArrayList<Day> history) {
+        this.history = new ArrayList<>(history);
+        Collections.reverse(this.history);
+    }
+
+    @NonNull
+    @Override
+    public HistoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.history_item, parent, false);
+        return new HistoryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HistoryViewHolder holder, int position) {
+        Day currentDay = history.get(position);
+
+        holder.currentDayDisplay.setText(currentDay.getName());
+        holder.dailyPointsDisplay.setText(String.valueOf(currentDay.getTotalPoints()));
+        holder.breakfastPointsDisplay.setText(String.valueOf(currentDay.getBreakfastPoints()));
+        holder.lunchPointsDisplay.setText(String.valueOf(currentDay.getLunchPoints()));
+        holder.dinnerPointsDisplay.setText(String.valueOf(currentDay.getDinnerPoints()));
+        holder.otherPointsDisplay.setText(String.valueOf(currentDay.getOtherPoints()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return history.size();
+    }
+
+    static class HistoryViewHolder extends RecyclerView.ViewHolder {
+        TextView currentDayDisplay;
+        TextView dailyPointsDisplay;
+        TextView breakfastPointsDisplay;
+        TextView lunchPointsDisplay;
+        TextView dinnerPointsDisplay;
+        TextView otherPointsDisplay;
+
+        public HistoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+            currentDayDisplay = itemView.findViewById(R.id.currentDayDisplay);
+            dailyPointsDisplay = itemView.findViewById(R.id.dailyPointsDisplay);
+            breakfastPointsDisplay = itemView.findViewById(R.id.breakfastPointsDisplay);
+            lunchPointsDisplay = itemView.findViewById(R.id.lunchPointsDisplay);
+            dinnerPointsDisplay = itemView.findViewById(R.id.dinnerPointsDisplay);
+            otherPointsDisplay = itemView.findViewById(R.id.otherPointsDisplay);
+        }
+    }
+}
