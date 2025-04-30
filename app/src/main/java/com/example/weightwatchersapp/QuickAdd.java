14bb0abc5bd36aa05fd75937f28883d43bd19065
Jack Touhey <jt112112@gmail.com>
2025-04-30 13:04:23 +1200

    #3 Added QuickAdd class

---
+++ app/src/main/java/com/example/weightwatchersapp/QuickAdd.java
@@ -0,0 +1,42 @@
+package com.example.weightwatchersapp;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "quickAdd")
+public class QuickAdd {
+    @PrimaryKey(autoGenerate = true)
+    private long quickAddID;
+    @ColumnInfo(name = "name")
+    private String name;
+    @ColumnInfo(name = "breakfast_points")
+    private Integer breakfastPoints;
+    @ColumnInfo(name = "lunch_points")
+    private Integer lunchPoints;
+    @ColumnInfo(name = "dinner_points")
+    private Integer dinnerPoints;
+    @ColumnInfo(name = "other_points")
+    private Integer otherPoints;
+
+    public void setQuickAddID(long quickAddID){this.quickAddID = quickAddID;}
+    public long getQuickAddID(){return this.quickAddID;}
+    public void setName(String name){this.name = name;}
+    public String getName(){return this.name;}
+    public void setBreakfastPoints(Integer breakfastPoints){this.breakfastPoints = breakfastPoints;}
+    public Integer getBreakfastPoints(){return this.breakfastPoints;}
+    public void setLunchPoints(Integer lunchPoints){
+        this.lunchPoints = lunchPoints;
+    }
+    public Integer getLunchPoints(){
+        return this.lunchPoints;
+    }
+    public void setDinnerPoints(Integer dinnerPoints){
+        this.dinnerPoints = dinnerPoints;
+    }
+    public Integer getDinnerPoints(){
+        return this.dinnerPoints;
+    }
+    public void setOtherPoints(Integer otherPoints){this.otherPoints = otherPoints;}
+    public Integer getOtherPoints(){return this.otherPoints;}
+}
